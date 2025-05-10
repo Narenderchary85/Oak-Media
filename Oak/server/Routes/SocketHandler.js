9139117f1d4baa3678fc888533f6bb7064cbbf00
Narenderchary85 <narenderchary85@gmail.com>
2025-05-10 12:07:51 +0530

    added

---
+++ Oak/server/Routes/SocketHandler.js
@@ -0,0 +1,67 @@
+import ChatModel from "../Models/ChatModel.js";
+
+export function socketHandler(io) {
+    io.on("connection", (socket) => {
+
+    socket.on("usersent",async(data)=>{
+            try{
+                const { senderId, reciverId, message, time, date } = data;
+
+                const members = [senderId, reciverId].sort();
+
+                let chat = await ChatModel.findOne({ members: { $all: members } });
+
+                if (!chat) {
+                chat = await ChatModel.create({
+                    members,
+                    messages: [{
+                    senderId,
+                    message,
+                    time,
+                    date
+                    }]
+                });
+            } else {
+                await ChatModel.updateOne(
+                    { _id: chat._id },
+                    {
+                    $push: {
+                        messages: {
+                        senderId,
+                        message,
+                        time,
+                        date
+                        }
+                    }
+                    }
+                );
+            }
+
+            io.emit('messagereceived', data);
+            }catch(err){
+                console.log(err);
+            }
+    })
+
+    socket.on('getChat', async ({ userId1, userId2 }) => {
+        try {
+          const chat = await ChatModel.findOne({
+            members: { $all: [userId1, userId2] }
+          });
+
+          if (chat) {
+            socket.emit('chatData', chat.messages); // send messages array
+          } else {
+            socket.emit('chatData', []); // no chat found
+          }
+        } catch (err) {
+          console.error(err);
+          socket.emit('chatError', 'Failed to fetch chat');
+        }
+      });
+
+      socket.on('disconnect', () => {
+        console.log('User disconnected');
+      });
+})
+}
--- Oak/server/app.js
+++ Oak/server/app.js
@@ -7,14 +7,36 @@ import jwt from "jsonwebtoken";
 import AuthRouter from './Routes/AuthRoute.js'
 import PostRouter from './Routes/PostRoute.js'
 import UserRouter from './Routes/UserRoute.js'
+import {Server} from 'socket.io';
+import { createServer } from 'http';
+import { socketHandler } from './Routes/SocketHandler.js';
 
 
 const app=express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    },
+});
+
 app.use(cors())
 dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
+
+io.on("connection", (socket) => {
+    console.log("Socket connected:", socket.id);
+  
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected:", socket.id);
+    });
+  });
+
+socketHandler(io);
 
 app.get('/',(req,res)=>{
     res.send("hello")
@@ -30,6 +52,7 @@ app.use('/auth',AuthRouter);
 app.use('/post',PostRouter);
 app.use('/user',UserRouter);
 
-app.listen(1000,()=>{
-    console.log("server connected")
-})
+const PORT = process.env.PORT || 1000;
+httpServer.listen(PORT, () => {
+  console.log(`Server connected on port ${PORT}`);
+});
